3554d88974710c6e7eaeaa51ff38c48e3410d034
Kyunghyun Cho <cho.k.hyun@gmail.com>
2013-07-22 11:29:49 +0300

    mnist convent example

--- example/example_mnist_convnet.m
+++ example/example_mnist_convnet.m
@@ -1,8 +1,8 @@
 % add the path of RBM code
 addpath('..');
 
-% load CIFAR-10
-load cifar_10.mat;
+% load MNIST
+mnist;
 
 % shuffle the training data
 perm_idx = randperm (size(X,1));
@@ -18,23 +18,14 @@ X_valid_labels = X_labels(perm_idx(n_train+1:end));
 X = X(perm_idx(1:n_train), :);
 X_labels = X_labels(perm_idx(1:n_train));
 
-use_whitening = 0;
-
-if use_whitening
-    %% ZCA
-    [Z, Wsep, Wmix, mX] = zca(X, 0.1);
-    X = zca_whiten(X, Wsep, Wmix, mX);
-    X_valid = zca_whiten(X_valid, Wsep, Wmix, mX);
-end
-
 % structures
-size_in = 32; % supports a squre image
-channel_in = 3; % grayscale image
-full_layers = [2000, 2000, 10];
-conv_layers = [5*5, 32, 5*5, 32]; % 32 5x5 filters x 2
-poolratios = [3,3]; % 3x3 pooling x 2
-pooling = [0, 1]; % max pooling + average pooling
-strides = [1,1]; % every pixel
+size_in = 28; % square image 28 x 28
+channel_in = 1; % grayscale image
+full_layers = [2000, 2000, 10]; % 2000-2000-10 fully connected layers
+conv_layers = [5*5, 32, 5*5, 32]; % 32 5x5 filters + 32 5x5 filters
+strides = [1,1]; % every pixel + every pixel
+pooling = [0, 1]; % max pooing + average pooling
+poolratios = [3,3]; % 3x3 pooling + 3x3 pooling
 
 % construct convnet
 C = default_convnet (size_in, channel_in, full_layers, conv_layers, poolratios, strides);
@@ -50,15 +41,10 @@ C.adadelta.use = 1;
 C.adadelta.momentum = 0.95;
 C.adadelta.epsilon = 1e-8;
 
-C.do_normalize = 1;
-C.do_normalize_std = 1;
+C.do_normalize = 0;
+C.do_normalize_std = 0;
 
-if use_whitening
-    D.do_normalize = 0;
-    D.do_normalize_std = 0;
-end
-
-C.dropout.use = 1;
+C.dropout.use = 0;
 
 C.learning.minibatch_sz = 32;
 
@@ -80,10 +66,10 @@ C.stop.criterion = 0;
 C.stop.recon_error.tolerate_count = 1000;
 
 % save the intermediate data after every epoch
-C.hook.per_epoch = {@save_intermediate, {'convnet_cifar10.mat'}};
+C.hook.per_epoch = {@save_intermediate, {'convnet_mnist.mat'}};
 
 % print learining process
-C.verbose = 1;
+C.verbose = 0;
 
 % train RBM
 fprintf(1, 'Training convnet\n');
@@ -91,9 +77,9 @@ tic;
 C = convnet (C, X, X_labels+1, X_valid, X_valid_labels+1, 0.1);
 fprintf(1, 'Training is done after %f seconds\n', toc);
 
-save('convnet_cifar10.mat', 'C');
+save('convnet_mnist.mat', 'C');
 
-[pred] = convnet_classify (C, X_test);
-n_correct = sum(X_test_labels+1 == pred);
 
-fprintf(2, 'Correctly classified test samples: %d/%d\n', n_correct, size(X_test, 1));
+
+
+
