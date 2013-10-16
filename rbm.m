be71e1eeba75693dd6d391452a341253c01e3d26
KyungHyunCho <KyunghyunCho@gyeonghyeon-joyi-macbook-pro.local>
2013-10-16 10:41:45 -0400

    removed unneccessary gather when use_gpu == 0

--- rbm.m
+++ rbm.m
@@ -462,7 +462,7 @@ for step=1:n_epochs
                             now_cost = sum(-dEs - logsum(double(gather(-fEs + cEs))) + log(size(vf,1)));
                             costs(s) = gather(now_cost);
                         else
-                            now_cost = sum(-dEs - logsum(gather(-fEs + cEs)) + log(size(vf,1)));
+                            now_cost = sum(-dEs - logsum(-fEs + cEs) + log(size(vf,1)));
                             costs(s) = now_cost;
                         end
                     end
